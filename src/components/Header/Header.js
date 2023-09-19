6669b36ab308de9731f64b087bd8adfbedb95ecd
julianddress <andres.mtta1@gmil.com>
2023-09-19 17:26:44 -0500

    CREATING COMPONENTS IN REACT

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css'
+
+// FORMA DE CREAR COMPONENTES EN REACT
+function Header(){
+    return  <header className='header'>
+                <img src="/img/header.png" alt="org"/>            
+            </header>
+    
+}
+
+// FORMA DE EXPORTAR COMPONENTES EN REACT
+export default Header
