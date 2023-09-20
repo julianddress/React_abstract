71db773b037c58cc57b8bbb0cefc51f8a5c6e1d3
julianddress <andres.mtta1@gmil.com>
2023-09-20 17:16:06 -0500

    PROPS - REQUIRED

--- src/components/Formulario/formulario.js
+++ src/components/Formulario/formulario.js
@@ -12,9 +12,9 @@ const Formulario = () => {
     return  <section className='formulario'>
                 <form onSubmit={manejoEnvio}>
                     <h2>Rellena el formulario para crear el colaborador</h2>
-                    <CampoTexto titulo='Nombre' placeholder='Registrar Nombre' />
-                    <CampoTexto titulo='Puesto' placeholder='Registrar Puesto'/>
-                    <CampoTexto titulo='Foto' placeholder='Registrar enlace de foto '/>
+                    <CampoTexto titulo='Nombre' placeholder='Registrar Nombre' required/>
+                    <CampoTexto titulo='Puesto' placeholder='Registrar Puesto' required/>
+                    <CampoTexto titulo='Foto' placeholder='Registrar enlace de foto ' required/>
                     <ListaOpciones/>
                     <Boton>Enviar</Boton>
                 </form>
