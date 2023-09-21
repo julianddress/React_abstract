f7995df66be8ac93575f068dc6eade2bc37d4878
julianddress <andres.mtta1@gmail.com>
2023-09-21 00:56:15 -0500

    USING AND UNDERSTANDING THE HOOKS CONCEPT

--- src/components/MiOrg/index.js
+++ src/components/MiOrg/index.js
@@ -1,9 +1,24 @@
+import { useState } from 'react'
 import './MiOrg.css'
 
+// ESTADOS - Hooks
+// useState()
+//           variable        funcion()
+// const [nombreVariable, funcionActualiza] = useState(valor);
+
 const MiOrg = () => {
+
+    // APLICAMOS EL HOOK
+    const [mostar, actualizarMostrar] = useState(true);
+
+    // CREAMOS UNA FUNCION PARA CONTROLAR EL EVENTO onClick DE LA IMG
+    const manejarClick = () => {
+        actualizarMostrar(!mostar)
+    }
+    
     return  <section className='orgSection'>
                 <h2 className='title'>Mi organización</h2>
-                <img src="./img/Agregar.png" alt="Boton Agregar"/>
+                <img src="./img/Agregar.png" alt="Boton Agregar" onClick={manejarClick}/>
             </section>
 }
 
