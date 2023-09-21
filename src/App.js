379278d9caec8feed702a7ae7669b57cbe6f177a
julianddress <andres.mtta1@gmail.com>
2023-09-21 01:41:57 -0500

    HOOKS, TERNARIOS, CORTOCIRCUITOS AND MORE

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 // FORMA DE IMPORTAR COMPONENTES EN REACT
@@ -6,14 +7,25 @@ import Formulario from './components/Formulario/formulario';
 import MiOrg from './components/MiOrg';
 
 function App() {
-  return (
-    <div>
 
-      {/* FORMA DE LLAMAR UNA FUNCION JSX */}
-      <Header/>
-      <Formulario />
-      <MiOrg/>
-    </div>
+  const [mostarFormulario, actualizarMostar] = useState(true);
+
+  // TERNARIO --> condicion ? seMuestra : noSeMuestra;
+  // CORTOCIRCUITO --> condicion && seMuestra
+
+  const cambiarMostar = () => {
+        actualizarMostar(!mostarFormulario)
+  }
+  return (
+          <div>
+
+            {/* FORMA DE LLAMAR UNA FUNCION JSX */}
+            <Header/>
+            {mostarFormulario === true ? <Formulario /> : <div></div>}
+            {mostarFormulario === true ? <Formulario /> : <></>}
+            {mostarFormulario && <Formulario />}
+            <MiOrg cambiarMostrar={cambiarMostar}/>
+          </div>
   );
 }
 
--- src/components/MiOrg/index.js
+++ src/components/MiOrg/index.js
@@ -1,24 +1,10 @@
-import { useState } from 'react'
 import './MiOrg.css'
 
-// ESTADOS - Hooks
-// useState()
-//           variable        funcion()
-// const [nombreVariable, funcionActualiza] = useState(valor);
+const MiOrg = (props) => {
 
-const MiOrg = () => {
-
-    // APLICAMOS EL HOOK
-    const [mostar, actualizarMostrar] = useState(true);
-
-    // CREAMOS UNA FUNCION PARA CONTROLAR EL EVENTO onClick DE LA IMG
-    const manejarClick = () => {
-        actualizarMostrar(!mostar)
-    }
-    
     return  <section className='orgSection'>
                 <h2 className='title'>Mi organización</h2>
-                <img src="./img/Agregar.png" alt="Boton Agregar" onClick={manejarClick}/>
+                <img src="./img/Agregar.png" alt="Boton Agregar" onClick={props.cambiarMostrar}/>
             </section>
 }
 
